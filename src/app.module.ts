67658fa6e57dd8a8bcfdbcb83fed012ba6c75c79
xzyDeathGun <1052515981@qq.com>
2022-06-28 16:18:40 +0800

    exception-filter-overview finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,11 @@ import {
   NestModule,
   RequestMethod,
 } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+// import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { CatsController } from './modules/cats/cats.controller';
 import { CatsModule } from './modules/cats/cats.module';
@@ -15,7 +18,12 @@ import { DatabaseModule } from './modules/database/database.module';
 @Module({
   imports: [CatsModule, DatabaseModule],
   controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  // 全局注入异常捕获方式1  方式2见main.ts
+  providers: [
+    { provide: APP_FILTER, useClass: AllExceptionsFilter },
+    AppService,
+    CatsService,
+  ],
 })
 export class AppModule implements NestModule {
   // 所有路由的中间件
